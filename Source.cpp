e9740343513ad0f0636d48f1246f2c2ddfd48849
Justin Hawe <101262383@georgebrown.ca>
2019-10-03 13:17:20 -0400

    Added Project1

--- Source.cpp
+++ Source.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 int main()
 {
-	int userInput;
-	int sum;
+	int num = 4;
 
-	while (true)// Infinite Loop
-	{
-		cout << "Enter a positive integer that will be added together. Type -1 to exit" << endl << endl;
-		cin >> userInput;
-		if (userInput == -1)
-		{
-			break; //Immediately exit out of the loop it currently is executing
-		}
-		sum = sum + userInput; // Sum += userInput 
-		cout << "Your added value is: " << sum << endl;
-	}
-
-	cout << "\nThat's All Flocks!\n";
+	cout << num		<< "   ";
+	cout << num++	<< "   ";
+	cout << num		<< "   ";
+	cout << ++num	<< "   ";
 
 	system("pause");
 
